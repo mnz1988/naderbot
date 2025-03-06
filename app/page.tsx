f24d2037d6d9350d658e43df6a05036bc371523b
Moji Norouzi zadeh <mnz1988@aol.com>
2025-03-07 02:43:45 +0330

    ui change

--- app/page.tsx
+++ app/page.tsx
@@ -91,9 +91,10 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 light:bg-gray-100 dark:bg-gray-800">
-      <Image src={myImageBot} alt="bot" className="mb-4 " width={100} height={100} />
-      <h1 className="text-2xl font-bold mb-4 mx-auto light:text-gray-800 dark:text-gray-100 ">برای عضویت در گروه های تخفیفی ویژه نجات غریقان و غواصان استان مرکزی ابتدا در کانال رسمی عضو شوید</h1>
-      <p/>
+      <Image src={myImageBot} alt="bot" width={100} height={100} />
+      <br/>
+      <h1 className="text-2xl font-bold mb-4 light:text-gray-800 dark:text-gray-100 ">برای عضویت در گروه های تخفیفی ویژه نجات غریقان و غواصان استان مرکزی ابتدا در کانال رسمی عضو شوید</h1>
+      <br/>
       <a href='https://t.me/taraah_net' className='text-xl font-medium light:text-gray-800 dark:text-gray-100'>@markazilifesaving 
       <span className="inline-flex items-baseline">
       <Image src={myImageExternalLink} alt="link" className="mx-1 size-4 self-center" />
